a2a9fb5f055482baba58e00fd897146da9308e82
Natacha Guillet <guillet.natacha@outlook.fr>
2018-05-25 01:19:20 -0400

    Food scroll

---
+++ Assets/Scripts/Travelator/FoodMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMove : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = GetComponentInParent<Travelator>().SpeedTravelator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/Travelator/Travelator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Travelator : MonoBehaviour {
+
+    [SerializeField] private float speedTravelator = 1f;
+    [SerializeField] private GameObject food;
+    private Transform spawnPoint;
+
+    public float SpeedTravelator
+    {
+        get
+        {
+            return speedTravelator;
+        }
+
+        set
+        {
+            speedTravelator = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        spawnPoint = GetComponentInChildren<Transform>();
+        InvokeRepeating("Spawn", speedTravelator, speedTravelator);
+    }
+
+    void Spawn()
+    {
+        // Create an instance of the food prefab at the spawn point's position and rotation.
+        GameObject newFood = Instantiate(food, spawnPoint.position, spawnPoint.rotation);
+        newFood.transform.parent = gameObject.transform;
+    }
+}
