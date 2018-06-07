133fc82cc676ac903881b1fd4c78b8d5894b0275
Natacha Guillet <guillet.natacha@outlook.fr>
2018-06-07 02:04:08 -0400

    added Ingredients and art work

---
+++ Assets/IngredientDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientDisplay : MonoBehaviour {
+
+    public Ingredient ingredient;
+
+    public Image artworkImage;
+
+    // Use this for initialization
+    void Start () {
+        artworkImage.sprite = ingredient.artwork;
+	}
+}
---
+++ Assets/Scripts/Food/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ingredient", menuName = "Ingredient")]
+public class Ingredient : ScriptableObject {
+
+    public new string name;
+    public int points;
+    public Sprite artwork;
+
+    public void Print()
+    {
+        Debug.Log(name + ": " + points + " points");
+    }
+}
