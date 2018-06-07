d9a76298099b9b163ab56eea06174c6b25d8b6e0
Dorian Boché <dorian.boche@outlook.fr>
2018-06-07 04:08:37 -0400

    last commit alpha

--- Assets/Scripts/Eat/Eat.cs
+++ Assets/Scripts/Eat/Eat.cs
@@ -6,21 +6,33 @@ public class Eat : MonoBehaviour {
 
     // Use this for initialization
     private bool inRange = false; // detect if the food can be eat or not
-    //[SerializeField] private Score score;
+    private Player player;
     private Score score;
-    void Start () {
+        void Start () {
         GameObject scoreGameObject = GameObject.FindWithTag("Score");
         if (scoreGameObject != null) score = scoreGameObject.GetComponent<Score>();
         else Debug.Log("Cannot find score GameObject");
-	}
+        GameObject playerGameObject = GameObject.FindWithTag("Player");
+        if (playerGameObject != null) player = playerGameObject.GetComponent<Player>();
+        else Debug.Log("Cannot find player GameObject");
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("space") && inRange) // if the player hit space and the food is in range, destroy the food
         {
+            if (gameObject.GetComponent<IngredientDisplay>().ingredient.points < 0)
+            {
+                score.ResetEatStreak();
+                player.ChangeHumor("puke");
+            }
+            else
+            {
+                score.AddEatStreak();
+                player.ChangeHumor("happy");
+            }
+            score.AddScore(gameObject.GetComponent<IngredientDisplay>().ingredient.points);
             Destroy(gameObject);
-            score.AddScore(1);
-            score.AddEatStreak();
         }
     }
 
---
+++ Assets/Scripts/Eat/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private string humor;
+    [SerializeField] private Sprite happy;
+    [SerializeField] private Sprite puke;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeFace();
+    }
+
+    public void ChangeFace()
+    {
+        switch(humor)
+        {
+            case "happy":
+                gameObject.GetComponent<SpriteRenderer>().sprite = happy;
+                break;
+            case "puke":
+                gameObject.GetComponent<SpriteRenderer>().sprite = puke;
+                break;
+        }
+    }
+
+    public void ChangeHumor(string newHumor)
+    {
+        humor = newHumor;
+    }
+}
