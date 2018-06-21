27fb13ea96e96e186d6afecab805d3ba08e36ec8
Natacha Guillet <guillet.natacha@outlook.fr>
2018-06-21 02:54:28 -0400

    save pseudo

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour {
 
+    [SerializeField]private TMP_InputField inputPseudo;
+    public static string pseudo;
+
     public void PlayGame() {
+        pseudo = inputPseudo.text;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Leaderboard() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
 }
